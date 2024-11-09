81c7107f681e3c9a669e4c75978045a017eef6fd
DSCruz98 <dscruz98@gmail.com>
2024-11-08 17:55:28 -0700

    Week 4-  4.2- Adding Sound
    Can't seem to upload folder for sounds. Folder should be called "Sounds".

---
+++ Sounds.js
@@ -0,0 +1,19 @@
+// Drake Cruz 11/8/24
+function sound(src) {
+  // add play function
+  this.sound = document.createElement("audio"); //create audio
+  this.sound.src = src; //define src
+  this.play = function () {
+    //music function
+    this.sound.play(); //play sound
+  };
+}
+// starts owl audio
+function play() {
+  mySound = new sound("Sounds/owl.mp3");
+  mySound.play();
+}
+// Just refreshes the page ending all sounds
+function stop() {
+  window.location.reload();
+}
